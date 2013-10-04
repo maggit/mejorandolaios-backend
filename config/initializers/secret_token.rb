3ad74968524f576ddaa68c8fdd9d18c24b3c84ae
Raquel Hernandez <maggit@maggit.net>
2013-10-04 12:35:58 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mejorandola::Application.config.secret_key_base = 'c1d8c49213d57aa93dc42875ccbb3bf5ea164f7b2011d6c9c2a939f1ec2021ab6e80632a90875e049bbfc00e6fccfdf37eb9907658e2107dec2088d5320f0fbd'
