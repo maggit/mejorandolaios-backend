edba056766abc3b865149a775599ffd7386b5490
Raquel Hernandez <maggit@maggit.net>
2013-10-04 15:58:47 -0400

    Excluding V1 from rails_admin

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -28,7 +28,7 @@ RailsAdmin.config do |config|
 
   # Exclude specific models (keep the others):
   # config.excluded_models = ['Admin', 'Course', 'User']
-  config.excluded_models = ['MejorandolaApi', 'Helpers::ApiHelpers','MejorandolaAPI::Courses', 'MejorandolaAPI::Status', 'V1::Courses', 'V1::Status']
+  config.excluded_models = ['MejorandolaApi', 'Helpers::ApiHelpers','MejorandolaAPI::Courses', 'MejorandolaAPI::Status', 'V1::Courses', 'V1::Status', 'V1::V1']
 
   # Include specific models (exclude the others):
   # config.included_models = ['Admin', 'Course', 'User']
